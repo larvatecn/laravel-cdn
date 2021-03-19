8981cbfdcde73e47b9fb02d46f29ab07b54a5f48
XuTongle <xutongle@gmail.com>
2021-03-19 11:56:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# laravel-cdn
+
+CDN extension for Laravel.
+
+## 环境需求
+
+- PHP >= 7.2
+
+## Installation
+
+```bash
+composer require larva/laravel-cdn -vv
+```
+
---
+++ src/Cdn.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms
+ * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
+ * @link http://www.larva.com.cn/
+ * @license http://www.larva.com.cn/license/
+ */
+
+namespace Larva\Cdn;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * CDN
+ * @author Tongle Xu <xutongle@gmail.com>
+ */
+class Cdn extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'cdn';
+    }
+}
